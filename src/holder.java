2bc2fa3909a6b1d017445ee561b797babf7c01fd
Will N <rsa.swimmer1@gmail.com>
2020-09-26 10:44:16 -0500

    Add java docs for GameZone.java

---
+++ src/holder.java
@@ -0,0 +1,65 @@
+/*
+try
+		{
+			do
+			{
+				System.out.println("Here are your options");
+				System.out.println("1: Twenty-One");
+				System.out.println("2: Rock Paper Scissors");
+				System.out.println("3: Exit");
+				choice = sc.nextInt();
+				if(choice == 1)
+				{
+					System.out.println("Launching--- Twenty-One");
+					twentyOne(sc, ran);
+				}//if
+				else if(choice == 2)
+				{
+					System.out.println("Launching--- Rock Paper Scisoors");
+					rps(sc, ran);
+				}
+			}while(choice != 3);
+		}catch(Exception ex)
+		{
+			System.out.println("Something went terrible wrong");
+		}
+*/
+/*
+ try
+			{
+				int user = ran.nextInt(11)+1;
+				System.out.printf("You drew %d \n", user);
+				total = total + user;
+				System.out.printf("Your current total is %d \n", total);
+				System.out.println("Would you like to draw again? ");
+				choice = sc.next();
+			}catch(Exception ex){
+				System.out.println("Something went terrible wrong");
+			}
+*/
+/*
+do
+		{
+			try
+			{
+				
+				System.out.println("Here are your options");
+				System.out.println("1: Twenty-One");
+				System.out.println("2: Rock Paper Scissors");
+				System.out.println("3: Exit");
+				choice = sc.nextInt();
+				if(choice == 1)
+				{
+					System.out.println("Launching--- Twenty-One");
+					twentyOne(sc, ran);
+				}//if
+				else if(choice == 2)
+				{
+					System.out.println("Launching--- Rock Paper Scisoors");
+					rps(sc, ran);
+				}
+			}catch(Exception ex) {
+				System.out.println("Something went terrible wrong");
+				System.out.println("main");
+			}
+*/
